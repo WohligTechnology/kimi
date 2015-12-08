7135b56a00a0546bd697b704394c10d3cd27c32c
TusharSachde <tushar@wohlig.com>
2015-12-08 18:11:55 +0530

    Section Figures

--- js/controllers.js
+++ js/controllers.js
@@ -8,14 +8,28 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.navigation = NavigationService.getnav();
 
   $scope.section = {
-    "one": "views/section/section1.html",
-    "two": "views/section/section2.html",
-    "three": "views/section/section3.html",
-    "four": "views/section/section4.html",
-    "five": "views/section/section5.html",
-    "six": "views/section/section6.html",
-    "seven": "views/section/section7.html",
+    one: "views/section/section1.html",
+    two: "views/section/section2.html",
+    three: "views/section/section3.html",
+    four: "views/section/section4.html",
+    five: "views/section/section5.html",
+    six: "views/section/section6.html",
+    seven: "views/section/section7.html",
   };
+
+  $scope.wedo = [{
+    id: 1,
+    title: 'Musicals',
+    src: 'img/musical.jpg'
+  },{
+    id: 2,
+    title: 'Theatre',
+    src: 'img/theatre.jpg'
+  },{
+    id: 3,
+    title: 'Concerts',
+    src: 'img/concerts.jpg'
+  }];
 
 })
 
